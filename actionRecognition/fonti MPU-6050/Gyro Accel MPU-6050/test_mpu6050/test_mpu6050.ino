a4d568086d8cae326df296150b2104932060909a
Massimiliano D'Amico <massimiliano.damico@exprivia.it>
2022-04-27 10:23:31 +0200

    new

---
+++ actionRecognition/fonti MPU-6050/Gyro Accel MPU-6050/test_mpu6050/test_mpu6050.ino
@@ -0,0 +1,104 @@
+// Roll, Pitch and Yaw with MPU6050
+// https://www.giuseppecaccavale.it
+// Giuseppe Caccavale
+
+#include <Wire.h>
+#include <MPU6050.h>
+
+MPU6050 mpu;
+
+// Pitch, Roll and Yaw values
+int pitch = 0;
+int roll = 0;
+float yaw = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println("Initialize MPU6050");
+
+  while (!mpu.begin(MPU6050_SCALE_2000DPS, MPU6050_RANGE_2G)) {
+    Serial.println("Could not find a valid MPU6050 sensor, check wiring!");
+    delay(500);
+  }
+
+  // Calibrate gyroscope. The calibration must be at rest.
+  // If you don't want calibrate, comment this line.
+  mpu.calibrateGyro();
+
+  // Set threshold sensivty. Default 3.
+  // If you don't want use threshold, comment this line or set 0.
+  mpu.setThreshold(1);
+
+  // Check settings
+  checkSettings();
+}
+
+void loop() {
+  // Read normalized values
+  Vector normAccel = mpu.readNormalizeAccel();
+  Vector normGyro = mpu.readNormalizeGyro();
+
+  // Calculate Pitch & Roll
+  pitch = -(atan2(normAccel.XAxis, sqrt(normAccel.YAxis * normAccel.YAxis + normAccel.ZAxis * normAccel.ZAxis)) * 180.0) / M_PI;
+  roll = (atan2(normAccel.YAxis, normAccel.ZAxis) * 180.0) / M_PI;
+
+  //Ignore the gyro if our angular velocity does not meet our threshold
+  if (normGyro.ZAxis > 1 || normGyro.ZAxis < -1) {
+    normGyro.ZAxis /= 100;
+    yaw += normGyro.ZAxis;
+  }
+
+  //Keep our angle between 0-359 degrees
+  if (yaw < 0)
+    yaw += 360;
+  else if (yaw > 359)
+    yaw -= 360;
+
+  // Output
+  Serial.print("Pitch = ");
+  Serial.print(pitch);
+  Serial.print("\tRoll = ");
+  Serial.print(roll);
+  Serial.print("\tYaw = ");
+  Serial.print(yaw);
+
+  Serial.println();
+
+  delay(10);
+}
+
+void checkSettings() {
+  Serial.println();
+
+  Serial.print(" * Sleep Mode:        ");
+  Serial.println(mpu.getSleepEnabled() ? "Enabled" : "Disabled");
+
+  Serial.print(" * Clock Source:      ");
+  switch (mpu.getClockSource()) {
+    case MPU6050_CLOCK_KEEP_RESET:     Serial.println("Stops the clock and keeps the timing generator in reset"); break;
+    case MPU6050_CLOCK_EXTERNAL_19MHZ: Serial.println("PLL with external 19.2MHz reference"); break;
+    case MPU6050_CLOCK_EXTERNAL_32KHZ: Serial.println("PLL with external 32.768kHz reference"); break;
+    case MPU6050_CLOCK_PLL_ZGYRO:      Serial.println("PLL with Z axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_YGYRO:      Serial.println("PLL with Y axis gyroscope reference"); break;
+    case MPU6050_CLOCK_PLL_XGYRO:      Serial.println("PLL with X axis gyroscope reference"); break;
+    case MPU6050_CLOCK_INTERNAL_8MHZ:  Serial.println("Internal 8MHz oscillator"); break;
+  }
+
+  Serial.print(" * Gyroscope:         ");
+  switch (mpu.getScale()) {
+    case MPU6050_SCALE_2000DPS:        Serial.println("2000 dps"); break;
+    case MPU6050_SCALE_1000DPS:        Serial.println("1000 dps"); break;
+    case MPU6050_SCALE_500DPS:         Serial.println("500 dps"); break;
+    case MPU6050_SCALE_250DPS:         Serial.println("250 dps"); break;
+  }
+
+  Serial.print(" * Gyroscope offsets: ");
+  Serial.print(mpu.getGyroOffsetX());
+  Serial.print(" / ");
+  Serial.print(mpu.getGyroOffsetY());
+  Serial.print(" / ");
+  Serial.println(mpu.getGyroOffsetZ());
+
+  Serial.println();
+}
